0ad70e06bbbe66ab42473f47fa66d43fb1ff3ca1
NurbolMarlisov <marlisaaliev195@gmail.com>
2022-12-15 21:49:05 +0600

    Initial commit

---
+++ app/src/main/java/com/example/dedicasion/SecondFragment.java
@@ -0,0 +1,71 @@
+package com.example.dedicasion;
+
+import static android.app.Activity.RESULT_OK;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class SecondFragment extends Fragment {
+    TextView textView;
+    ImageView imageView;
+    int SELECT_PICTURE = 200;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        textView = requireActivity().findViewById(R.id.textView);
+        imageView = requireActivity().findViewById(R.id.imageView);
+        if (getArguments() !=null){
+            textView.setText(getArguments().getString("key"));
+        }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageChooser();
+            }
+        });
+    }
+
+    void imageChooser() {
+
+        Intent i = new Intent();
+        i.setType("image/*");
+        i.setAction(Intent.ACTION_GET_CONTENT);
+        //docu
+        startActivityForResult(Intent.createChooser(i, "Select Picture"), SELECT_PICTURE);
+    }
+    //doku
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //doku
+        if (resultCode == RESULT_OK) {
+
+            if (requestCode == SELECT_PICTURE) {
+
+                Uri selectedImageUri = data.getData();
+                if (null != selectedImageUri) {
+                    imageView.setImageURI(selectedImageUri);
+                }
+            }
+        }
+    }
+}
